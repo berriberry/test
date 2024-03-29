440249e587cc24f4471a0e5b3ab5921354359580
berriberry <157345608+berriberry@users.noreply.github.com>
2024-03-29 14:36:34 +0900

    Add files via upload

---
+++ nat-instance.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+sudo yum install -y iptables-services
+sudo systemctl start iptables.service
+sudo systemctl enable iptables.servcie
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo service iptables save
